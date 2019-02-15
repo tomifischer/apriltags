76cf1919e0213a49f4063c03927b1a930d88b607
Thomas Fischer <tfischer@dc.uba.ar>
2019-02-15 19:47:59 -0300

    export pose prediction for C++

--- include/apriltags/apriltag_pose.h
+++ include/apriltags/apriltag_pose.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "apriltag.h"
 #include "common/matd.h"
 
@@ -64,4 +68,6 @@ void estimate_tag_pose_orthogonal_iteration(
  */
 double estimate_tag_pose(apriltag_detection_info_t* info, apriltag_pose_t* pose);
 
-
+#ifdef __cplusplus
+}
+#endif
